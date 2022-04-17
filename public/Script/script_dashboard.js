531efe3911ada33351b23e8b6bcdb4c9ac04e36a
Matheus <matheus.patricio@sptech.school>
2022-04-17 11:38:40 -0300

    Merge branch 'main' of https://github.com/Systemp-PI/Site-Institucional

--- public/Script/script_dashboard.js
+++ public/Script/script_dashboard.js
@@ -1,7 +1,7 @@
 function cadastrarMaquina(){
  var formulario = document.getElementById("ocultar") 
  formulario.style.visibility='visible'
- formulario.style.transition='opacity 1s linear';
+ formulario.style.transition='opacity 0.5s linear';
  formulario.style.opacity='1';
  
 }

eae72947e69c18ff32d3a37494a9b1b804503d07
Matheus <matheus.patricio@sptech.school>
2022-04-22 18:40:51 -0300

    Automatização da dashboard por cliques

--- public/Script/script_dashboard.js
+++ public/Script/script_dashboard.js
@@ -17,7 +17,9 @@ function enviar_dados_maquina() {
     temperatura_max = Number(input_temperatura_max.value);
     temperatura_min = Number(input_temperatura_min.value);
 }
-function mostrar_dados_dashboard(idMaq, vetor) {
+function mostrar_dados_dashboard(idMaq, vetor,classe,myChart) {
+    var dashboard = document.getElementById(classe)
+    dashboard.style.display = 'block'
     var maquina = document.getElementById(idMaq)
     maquina.addEventListener('click', operarHTML(vetor))
     function operarHTML(valores) {
@@ -26,15 +28,15 @@ function mostrar_dados_dashboard(idMaq, vetor) {
         span_ideal.innerHTML = valores[2] + "°C"
         span_alta.innerHTML = valores[3] + "°C"
         span_m_alta.innerHTML = valores[4] + "°C"
-        addData([37.8,37.9,38,38.2,38.5])
+        addData(myChart,[37.8,37.9,38,38.2,38.5])
     }
-    function addData(data) {
-        myChart.data.labels.push(new Date().toLocaleTimeString())
-        myChart.data.datasets[0].data.push(data[0]);
-        myChart.data.datasets[0].data.push(data[1]);
-        myChart.data.datasets[0].data.push(data[2]);
-        myChart.data.datasets[0].data.push(data[3]);
-        myChart.data.datasets[0].data.push(data[4]);
-        myChart.update();
+    function addData(myChart,data) {
+       myChart.data.labels.push(new Date().toLocaleTimeString())
+       myChart.data.datasets[0].data.push(data[0]);
+       myChart.data.datasets[0].data.push(data[1]);
+       myChart.data.datasets[0].data.push(data[2]);
+       myChart.data.datasets[0].data.push(data[3]);
+       myChart.data.datasets[0].data.push(data[4]);
+       myChart.update();
     }
 }

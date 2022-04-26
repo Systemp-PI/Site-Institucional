7d3c20fa57683ef2d81b97eec60ec6c216842508
Pedro <pedro.pires@sptech.school>
2022-04-25 22:57:19 -0300

    Alterção nas fotos dos colaboradores

--- src/database/config.js
+++ src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     user: "root",
     database: "systemp",
-    password: "mauricio123",
+    password: "brasil12",
 };
 
 function executar(instrucao) {

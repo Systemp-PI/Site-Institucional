90abd4dd2edff3f7e8e6169d10089deeac4c6a4f
Matheus <matheus.patricio@sptech.school>
2022-05-18 20:56:45 -0300

    Funções de mostrar o grafico no html add

--- public/Script/script_dashboard.js
+++ public/Script/script_dashboard.js
@@ -10,7 +10,7 @@ var spanValor = document.getElementById('valorSpan')
 var divMaquina = document.querySelector(`maq1`)
 var divImagem = document.querySelector(`.redutor_img`)
 var divClick = document.querySelector('.identificacao_maq')
-divClick.onclick= mostrar_dados_dashboard()
+
 abrir_tela.onclick = function cadastrarMaquina() {
     tela_cadastro.style.display = "block";
 }
@@ -34,16 +34,11 @@ function listarMaquinas() {
         resposta.json().then(function (resultado) {
             console.log('Esta maquina tem temp min::' + resultado[0, 2].temp_min)
 
-            const temp_alert_min = ((Number( resultado[0, 2].temp_max) - Number( resultado[0, 2].temp_min)) * 25) / 100;
-            const temp_ideal = ((Number(resultado[0, 2].temp_max) - Number( resultado[0, 2].temp_min)) * 50) / 100;
-            const temp_alert_max = ((Number(resultado[0, 2].temp_max) - Number( resultado[0, 2].temp_min)) * 75) / 100;
-            let kpi = new Array(temp_min_crit, temp_alert_min, temp_ideal, temp_alert_max, temp_max_crit)
             for (var i = 0; i < resultado.length; i++) {
                 clonar(resposta.nome_maquina)
-                console.log(resultado[i, i].nome_maquina)
-                console.log(kpi)
+                console.log(resultado[i, i].nome_maquina+"temp_min:"+resultado[i, i].temp_min,+"temp_max:"+resultado[i, i].temp_max)
                 spanValor.innerHTML = resultado[i, i].nome_maquina;
-                mostrar_dados_dashboard(resultado[i, i].nome_maquina, kpi, 'grafico1', myChart1, 'myChartMedia')
+
             }
 
         })
@@ -103,55 +98,3 @@ cadastrar_Maquinas.onclick = function cadastrarMaquinas() {
     return false;
 }
 
-
-function mostrar_dados_dashboard(idMaquina, vetor, div_grafico, myChart, myChartMedia) {
-    console.log(intervalo)
-    if (intervalo !== undefined) {
-        clearInterval(intervalo)
-    }
-    intervalo = setInterval(function () {
-        adicionar_dados(idMaquina, vetor, div_grafico, myChart, myChartMedia)
-
-    }, 2000)
-    console.log(intervalo)
-
-}
-
-//Exibir os gráficos da máquina selecionada pelo usuário
-function adicionar_dados(idMaquina, valores, div_grafico, myChart, myChartMedia) {
-    var dashboard = document.getElementById(div_grafico)
-    var dashboardMedia = document.getElementById(myChartMedia)
-    esconder_graficos()
-    operarHTML(valores)
-    dashboard.style.display = 'block'
-    dashboardMedia.style.display = 'block'
-
-    function operarHTML(valores) {
-        span_m_baixa.innerHTML = valores[0] + "°C"
-        span_baixa.innerHTML = valores[1] + "°C"
-        span_ideal.innerHTML = valores[2] + "°C"
-        span_alta.innerHTML = valores[3] + "°C"
-        span_m_alta.innerHTML = valores[4] + "°C"
-        addData(myChart, [37.8, 40.5, 45, 43, 39.5])
-    }
-    function addData(myChart, temperatura) {
-        myChart.data.labels.push(new Date().toLocaleTimeString())
-        myChart.data.datasets[0].data.push(temperatura[0]);
-        myChart.data.datasets[0].data.push(temperatura[1]);
-        myChart.data.datasets[0].data.push(temperatura[2]);
-        myChart.data.datasets[0].data.push(temperatura[3]);
-        myChart.data.datasets[0].data.push(temperatura[4]);
-        myChart.update();
-    }
-    if (myChart.data.labels.length == 10) {
-        myChart.data.labels.shift();
-        myChart.data.datasets[0].data.shift();
-    }
-    myChart.update();
-
-    //Ocultar os demais gráficos da dashboard
-    function esconder_graficos() {
-        document.getElementById('grafico1').style.display = 'none'
-        document.getElementById('graficoMedia').style.display = 'none'
-    }
-}

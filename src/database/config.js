9074a96a694be18a7662775387af968808c35f75
Gabriel Nogueira <gabriel.costa@sptech.school>
2022-05-26 14:00:42 -0300

    atualização api e tela cadastro empresa

--- src/database/config.js
+++ src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     user: "root",
     database: "systemp",
-    password: "",
+    password: "Gabriel@2002",
 };
 
 function executar(instrucao) {
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -31,13 +31,13 @@ function cadastrar(nome, cnpj, email, senha) {
     return database.executar(instrucao);
 }
 
-function cadastrar_empresa(razao, cnpj, insc_estadual, nomecont1, emailcont1, telcont1, nomecont2, emailcont2, telcont2,cep, estado, cidade , bairro ,rua ,numero,complemento) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar_empresa():",razao, cnpj, insc_estadual, nomecont1, emailcont1, telcont1, nomecont2, emailcont2, telcont2,cep,estado, cidade, bairro ,rua ,numero,complemento );
+function cadastrar_empresa(razao, cnpj, insc_estadual, nomecont1, emailcont1, telcont1, nomecont2, emailcont2, telcont2, cep, estado, cidade, bairro, rua, numero, complemento) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar_empresa():",razao, cnpj, insc_estadual, nomecont1, emailcont1, telcont1, nomecont2, emailcont2, telcont2, cep, estado, cidade, bairro ,rua ,numero,complemento );
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO empresas (razao_social, cnpj, inscricao_estadual, contato_nome, contato_email, contato_tel, contato2_nome, contato2_email, contato2_tel, cep, estado, cidade, bairro, rua, numero, complemento) VALUES ('${razao}', '${cnpj}' ,'${insc_estadual}', '${nomecont1}', '${emailcont1}', '${telcont1}', '${nomecont2}', '${emailcont2}', '${telcont2}', '${cep}', '${estado}', '${cidade}', '${bairro}, '${rua}', '${numero}', '${complemento}');
+        INSERT INTO empresas (razao_social, cnpj, inscricao_estadual, contato1_nome, contato1_email, contato1_tel, contato2_nome, contato2_email, contato2_tel, cep, estado, cidade, bairro, rua, numero, complemento) VALUES ('${razao}', '${cnpj}' ,'${insc_estadual}', '${nomecont1}', '${emailcont1}', '${telcont1}', '${nomecont2}', '${emailcont2}', '${telcont2}', '${cep}', '${estado}', '${cidade}', '${bairro}', '${rua}', '${numero}', '${complemento}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
